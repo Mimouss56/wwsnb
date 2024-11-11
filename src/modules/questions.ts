7272d94310e8939f628e55d643f191b5f2ba69f1
Mouss <lepriol.matthieu@gmail.com>
2024-11-11 14:46:55 +0100

    Merge branch 'feature/review_moderator'

--- src/modules/questions.ts
+++
@@ -1,8 +0,0 @@
-export function addClassQuestion(message: HTMLElement) {
-    const messageContainer = message.closest('.sc-leYdVB');
-    // Add highlight class if not already present
-    messageContainer 
-    && !messageContainer.classList.contains('question-highlight') 
-    && messageContainer.classList.add('question-highlight');
-
-}
