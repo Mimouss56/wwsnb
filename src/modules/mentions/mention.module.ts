8867ece31f914197697e3b4c9a7d841e78f78b34
Mouss <lepriol.matthieu@gmail.com>
2024-11-11 14:46:01 +0100

    refactor modo module + apply same structure for question and mention

--- src/modules/mentions/mention.module.ts
+++ src/modules/mentions/mention.module.ts
@@ -1,5 +1,14 @@
+import { getActualUserName } from "../users/user.module";
+
 export function addClassMention(message: HTMLElement) {
     // Add special styling class
     const messageContainer = message.closest('.sc-leYdVB');
     messageContainer && !messageContainer.classList.contains('mention-highlight') && messageContainer.classList.add('mention-highlight');
 }
+
+export function checkForMentions(message: HTMLElement, textContent: string | null) {
+    const actualUserName = getActualUserName();
+    if (textContent?.includes(`@${actualUserName}`)) {
+      addClassMention(message);
+    }
+  }
